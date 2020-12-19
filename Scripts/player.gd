5a84ae42203eceb9430cc4ff9f4dd2192c171134
GodePTPM <biganoob@gmail.com>
2020-12-19 12:10:49 +0100

    fixed deceleration

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,11 +10,14 @@ var currentSpeed = 0.0
 
 var accelerateInterpolation = 0.0
 var decelerateInterpolation = 0.0
+
 var prevVector = Vector3(0,0,0)
 
 func _ready():
 	cameraSpatial = get_node("camera_spatial")
 	kinematicBody = get_node("KinematicBody")
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
 	accelerateInterpolation = clamp(accelerateInterpolation+delta, 0, 1)
@@ -32,20 +35,17 @@ func _process(delta):
 	if Input.is_action_pressed("walk_backward"):
 		moveVector += kinematicBody.get_global_transform().basis.z*currentSpeed; moving = true
 	
-	if moving != true:
-		accelerateInterpolation = 0
-		currentSpeed = lerp(currentSpeed, 0, decelerateInterpolation)
-	else:
-		decelerateInterpolation = 0
-		prevVector = moveVector - Vector3(0,-5,0)
-		currentSpeed = lerp(currentSpeed, charSpeed, accelerateInterpolation)
-	
 	kinematicBody.set_rotation(Vector3(0, cameraSpatial.get_rotation().y, 0))
 	
-	if moving != true:
+	if !moving:
+		accelerateInterpolation = 0
+		currentSpeed = lerp(currentSpeed, 0, decelerateInterpolation)
 		kinematicBody.move_and_slide(moveVector+(prevVector*currentSpeed), Vector3.UP)
 	else:
+		decelerateInterpolation = 0
+		currentSpeed = lerp(currentSpeed, charSpeed, accelerateInterpolation)
 		kinematicBody.move_and_slide(moveVector, Vector3.UP)
+		prevVector = (moveVector-Vector3(0,-5,0))/currentSpeed
 	
 	cameraSpatial.translation = kinematicBody.translation
 
